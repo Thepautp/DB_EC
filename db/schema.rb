6abdd860fc95647cd06beb304261fd9f55215720
thepaut <p-thepaut@ankh-systems.co.jp>
2018-04-11 17:45:31 +0900

    add order_detail table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411083530) do
+ActiveRecord::Schema.define(version: 20180411084349) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "zip"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 20180411083530) do
 
   create_table "fukens", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "product_id"
+    t.integer "quantity"
+    t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
