37075e87d5a3a0643dfabf66b58a0c7e6f96be7b
thepaut <p-thepaut@ankh-systems.co.jp>
2018-04-11 17:38:04 +0900

    add order_status table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411083151) do
+ActiveRecord::Schema.define(version: 20180411083530) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "zip"
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 20180411083151) do
 
   create_table "fukens", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_statuses", force: :cascade do |t|
+    t.integer "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
