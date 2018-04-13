95bb6ee5d00145a39ef62651f333dac691cfe14f
thepaut <p-thepaut@ankh-systems.co.jp>
2018-04-13 10:28:41 +0900

    add basics controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  root 'home_page#home'
+  
+  get 'home_page/home'
+
+  get 'suppliers/index'
+
+  get 'suppliers/show'
+
+  get 'customers/index'
+
+  get 'customers/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
