3892e20163c1cec27e8e5338645182d838c6252e
thepaut <p-thepaut@ankh-systems.co.jp>
2018-04-17 16:10:29 +0900

    add best seller and soon sold out product on home page

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,5 +1,13 @@
 class HomePageController < ApplicationController
   def home
-    @categories = ProdCategorie.all    
+    @categories = ProdCategorie.all
+    sorted_products = OrderDetail.group(:product_id).count
+    ordered_products = Hash[sorted_products.sort_by{|k,v| v}.reverse].keys
+    all_ninki_product = Product.find(ordered_products.first(5))
+    @best_seller = all_ninki_product.delete_at(0)
+    @ninki_product = all_ninki_product
+    products = Product.find(ordered_products)
+    @soon_sold_out = products.delete_if {|prod| prod.stock >=5 || prod.stock == 0}
   end
 end
+ 
