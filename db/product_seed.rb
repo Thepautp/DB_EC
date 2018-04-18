b59d6074fd204b8a4804add3d8ce7b369b38bb5d
thepaut <p-thepaut@ankh-systems.co.jp>
2018-04-18 13:20:11 +0900

    fix seed

--- db/product_seed.rb
+++ db/product_seed.rb
@@ -5,7 +5,9 @@ suppliers.each do |s|
     product_name = (0...8).map { (65 + rand(26)).chr }.join.downcase!
     price = (rand(100) * rand).round 2
     weight = ((rand(100) * rand)+0.1).round(2)
-    s.products.create(name: product_name, prod_categorie_id: (rand(7)+1), prod_type_id: (rand(43)+1),
-    price: price, stock: rand(100), weight: weight)    
+    cat = rand(7)+1
+    type = rand(43)+1
+    stock = rand(100)
+    s.products.create(name: product_name, prod_categorie_id: cat, prod_type_id: type, price: price, stock: stock, weight: weight) 
   end
 end
--- db/users_seeds.rb
+++ db/users_seeds.rb
@@ -1,6 +1,5 @@
 20.times do |i|
-  a = Customer.create(email: "customer_#{i}@test.bzh", first_name: "Louis_#{i}", last_name: "Durant_#{i}",
-                  fn_furigana: "ルイ_#{i}", ln_furigana: "デュラン_#{i}")
+  a = Customer.create(email: "customer_#{i}@test.bzh", first_name: "Louis_#{i}", last_name: "Durant_#{i}", fn_furigana: "ルイ_#{i}", ln_furigana: "デュラン_#{i}")
   a.create_customer_address(zip: "#{rand(999)}-#{rand(9999)}", fuken_id: (rand(47)+1), addr_1: "city_#{rand(5)+1}", addr_2: "#{rand(5)+1}-#{rand(20)+1}-#{rand(20)+1}")
 end
 20.times do |i|
