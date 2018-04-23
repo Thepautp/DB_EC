3013293ea3222646f68863ccce7b3411494b072e
thepaut <p-thepaut@ankh-systems.co.jp>
2018-04-23 11:43:59 +0900

    set local lang and default param

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
+  
+  
+  def set_locale
+    I18n.locale = extract_locale_from_tld || I18n.default_locale
+    p"Locale set: #{I18n.locale}"
+  end
+  def default_url_options
+    { lang: I18n.locale }
+  end
+ 
+  # Get locale from top-level domain or return +nil+ if such locale is not available
+  # You have to put something like:
+  #   127.0.0.1 application.com
+  #   127.0.0.1 application.it
+  #   127.0.0.1 application.pl
+  # in your /etc/hosts file to try this out locally
+  def extract_locale_from_tld
+    parsed_locale = request.host.split('.').last
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
+
 end
