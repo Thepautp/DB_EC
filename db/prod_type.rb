0c70c944871276f52d7add791f8039670ee1a288
thepaut <p-thepaut@ankh-systems.co.jp>
2018-04-24 19:16:31 +0900

    fix PRodType seed for populate translation

--- db/prod_type.rb
+++ db/prod_type.rb
@@ -60,3 +60,106 @@ type_for_connectique.each do |type|
   cat_id = ProdCategorie.where(name: "Connectique").first.id
   a = ProdType.create(name: type, prod_categorie_id: cat_id)
 end
+
+I18n.locale = :fr
+
+type = ProdType.where(prod_categorie_id: 1)
+type.each_with_index do |t,i|
+  t.name = type_for_informatique[i]
+  t.save  
+end
+type = ProdType.where(prod_categorie_id: 2)
+type.each_with_index do |t,i|
+  t.name = type_for_image_et_son[i]
+  t.save  
+end
+type = ProdType.where(prod_categorie_id: 3)
+type.each_with_index do |t,i|
+  t.name = type_for_telephonie[i]
+  t.save  
+end
+type = ProdType.where(prod_categorie_id: 4)
+type.each_with_index do |t,i|
+  t.name = type_for_jeux[i]
+  t.save  
+end
+type = ProdType.where(prod_categorie_id: 5)
+type.each_with_index do |t,i|
+  t.name = type_for_objects_connectes[i]
+  t.save  
+end
+type = ProdType.where(prod_categorie_id: 6)
+type.each_with_index do |t,i|
+  t.name = type_for_consommables[i]
+  t.save  
+end
+type = ProdType.where(prod_categorie_id: 7)
+type.each_with_index do |t,i|
+  t.name = type_for_connectique[i]
+  t.save  
+end
+
+type_for_informatique_en = [
+  "Computer Parts", "PC peripheral", "Laptop",
+  "Desktop", "Tablet", "Network", "Software",
+  "Point of Sale Equipment"
+]
+type_for_image_et_son_en = [
+  "Television", "Camera", "Projection", "Home theater & HiFi",
+   "Digital Sound", "Recorder", "Camcorder", "Furniture & Support",
+   "Dynamic dispaly"
+]
+type_for_telephonie_en = [  
+  "Mobile", "telephony & VoIP", "Cars", "Walkie talkie"
+]
+type_for_jeux_en = [
+  "Console", "Games", "Console Accessories and PC", "Geek Zone"
+]
+type_for_objects_connectes_en = [  
+  "Leisure", "House", "Health", "Sport"
+]
+type_for_consommables_en = [
+  "Printer consumable", "CD / DVD / Blu-ray", "Stationery", "Battery, battery",
+     "Cleaning", "CD / DVD sleeve", "Label, jacquette"
+]
+type_for_connectique_en = [
+  "PC Connectivity", "Network Connectivity", "Power Connectivity", "Hi-Fi Connectivity",
+   "Tools", "TV / Video Connections", "Storage"
+]
+I18n.locale = :en
+
+type = ProdType.where(prod_categorie_id: 1)
+type.each_with_index do |t,i|
+  t.name = type_for_informatique_en[i]
+  t.save  
+end
+type = ProdType.where(prod_categorie_id: 2)
+type.each_with_index do |t,i|
+  t.name = type_for_image_et_son_en[i]
+  t.save  
+end
+type = ProdType.where(prod_categorie_id: 3)
+type.each_with_index do |t,i|
+  t.name = type_for_telephonie_en[i]
+  t.save  
+end
+type = ProdType.where(prod_categorie_id: 4)
+type.each_with_index do |t,i|
+  t.name = type_for_jeux_en[i]
+  t.save  
+end
+type = ProdType.where(prod_categorie_id: 5)
+type.each_with_index do |t,i|
+  t.name = type_for_objects_connectes_en[i]
+  t.save  
+end
+type = ProdType.where(prod_categorie_id: 6)
+type.each_with_index do |t,i|
+  t.name = type_for_consommables_en[i]
+  t.save  
+end
+type = ProdType.where(prod_categorie_id: 7)
+type.each_with_index do |t,i|
+  t.name = type_for_connectique_en[i]
+  t.save  
+end
