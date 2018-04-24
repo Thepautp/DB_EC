48b81c91d4e27541526567b2aaee316bd47f77bb
thepaut <p-thepaut@ankh-systems.co.jp>
2018-04-24 17:45:28 +0900

    add jp seed

--- db/categorie.rb
+++ db/categorie.rb
@@ -23,10 +23,21 @@ end
 I18n.locale = :en
 prod_categorie_list_en = [
   "computer", "Picture and sound", "Telephony", "Games and leisure",
-  "Connected objects", "Consumables", "connectors"
+  "Connected objects", "Consumables", "Connectors"
 ]
 categories = ProdCategorie.all
 categories.each_with_index do |cat,i|
   cat.name = prod_categorie_list_en[i]
+  cat.save
+end
+
+I18n.locale = :jp
+prod_categorie_list_jp = [
+  "コンピューター", "映像と音声", "電話", "ゲームとレジャー",
+  "接続されたオブジェクト", "消耗品", "コネクタ"
+]
+categories = ProdCategorie.all
+categories.each_with_index do |cat,i|
+  cat.name = prod_categorie_list_jp[i]
   cat.save
 end
