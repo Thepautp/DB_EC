1e887378842f995df0dc529f67e68818909332b2
thepaut <p-thepaut@ankh-systems.co.jp>
2018-04-25 11:12:02 +0900

    add type jp translation

--- db/prod_type.rb
+++ db/prod_type.rb
@@ -163,3 +163,67 @@ type.each_with_index do |t,i|
   t.name = type_for_connectique_en[i]
   t.save  
 end
+
+type_for_informatique_jp = [
+  "コンピュータ部品","PC周辺機器","ラップトップ", "デスクトップ",
+  "タブレット","ネットワーク","ソフトウェア", "販売時点管理装置"
+]
+type_for_image_et_son_jp = [
+  "テレビ","カメラ","プロジェクション","ホームシアター＆オーディオ",
+  "デジタルサウンド","レコーダー","ビデオカメラ","家具とサポート",
+  "ダイナミックディスプレイ"
+]
+type_for_telephonie_jp = [  
+  "モバイル","電話＆VoIP","車","トランシーバー"
+]
+type_for_jeux_jp = [
+  "コンソール", "ゲーム", "コンソールアクセサリーとPC", "オタクゾーン"
+]
+type_for_objects_connectes_jp = [  
+  "レジャー", "ハウス", "健康", "スポーツ"
+]
+type_for_consommables_jp = [
+  "プリンタ消耗品", "CD / DVD / Blu-ray", "ステーショナリー", "バッテリー、バッテリー",
+  "クリーニング","CD / DVDスリーブ","ラベル、ジャケット"
+]
+type_for_connectique_jp = [
+  "PC接続","ネットワーク接続","電源接続","Hi-Fi接続","ツール",
+  "TV /ビデオ接続","ストレージ"
+]
+I18n.locale = :jp
+
+type = ProdType.where(prod_categorie_id: 1)
+type.each_with_index do |t,i|
+  t.name = type_for_informatique_jp[i]
+  t.save  
+end
+type = ProdType.where(prod_categorie_id: 2)
+type.each_with_index do |t,i|
+  t.name = type_for_image_et_son_jp[i]
+  t.save  
+end
+type = ProdType.where(prod_categorie_id: 3)
+type.each_with_index do |t,i|
+  t.name = type_for_telephonie_jp[i]
+  t.save  
+end
+type = ProdType.where(prod_categorie_id: 4)
+type.each_with_index do |t,i|
+  t.name = type_for_jeux_jp[i]
+  t.save  
+end
+type = ProdType.where(prod_categorie_id: 5)
+type.each_with_index do |t,i|
+  t.name = type_for_objects_connectes_jp[i]
+  t.save  
+end
+type = ProdType.where(prod_categorie_id: 6)
+type.each_with_index do |t,i|
+  t.name = type_for_consommables_jp[i]
+  t.save  
+end
+type = ProdType.where(prod_categorie_id: 7)
+type.each_with_index do |t,i|
+  t.name = type_for_connectique_jp[i]
+  t.save  
+end
