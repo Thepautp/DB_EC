aa1cbc88d66cf75931c7acda36643025d99e18fa
thepaut <p-thepaut@ankh-systems.co.jp>
2018-04-25 13:01:48 +0900

    fix unvalid locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   
   
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = params[:locale].to_sym.presence_in(I18n::available_locales) || I18n.default_locale
     p"Locale set: #{I18n.locale}"
   end
   def default_url_options
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module DBProject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    I18n.available_locales = %i(fr en jp)
+    I18n.enforce_available_locales = true
+    I18n.default_locale = :fr
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
