1a99d22d4683371782f4012e326ae5b9b29ce8fc
thepaut <p-thepaut@ankh-systems.co.jp>
2018-04-26 13:14:20 +0900

    install and init bcrypt

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,5 +2,6 @@ class Customer < ApplicationRecord
   has_many :orders
   has_one :customer_address, dependent: :destroy
   has_many :order_details, through: :orders
+  has_secure_password
   paginates_per 1
 end
